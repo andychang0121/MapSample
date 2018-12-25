6884e5f80cdf2d191be1b080fb1631384e63fc45
安迪學長 <andy_chang_0121@hotmail.com>
2018-12-25 10:46:13 +0800

    Add Plugins and some package Setup bower

--- LeafLets/App_Start/RouteConfig.cs
+++ LeafLets/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LeafLets
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Demo", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
